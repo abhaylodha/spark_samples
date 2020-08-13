73887f5538972ccc0554016f9f673f88a2d1ce0e
Abhaykumar Lodha <abhaykumar.lodha@gmail.com>
2020-08-13 14:44:06 +0530

    To handle any depth of grouping

--- src/main/scala/my/spark/CommonGroup.scala
+++ src/main/scala/my/spark/CommonGroup.scala
@@ -15,6 +15,8 @@ import org.apache.spark.sql.functions.{
 }
 import my.spark.common_utils.Logger
 import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.DataFrame
+import scala.annotation.tailrec
 
 object CommonGroup extends SparkRunner with Logger {
   def run(spark: SparkSession, args: Array[String]): Unit = {
@@ -31,7 +33,14 @@ object CommonGroup extends SparkRunner with Logger {
       ("TR5", "TG2"),
       ("TR6", "TG6"),
       ("TR7", "TG2"),
-      ("TR8", "TG1")).toDF("trade_ref_id", "trade_ref_group")
+      ("TR8", "TG1"),
+      ("TR9", "TG9"),
+      ("TR9", "TG10"),
+      ("TR10", "TG10"),
+      ("TR10", "TG11"),
+      ("TR11", "TG11"),
+      ("TR11", "TG12"),
+      ("TR12", "TG12")).toDF("trade_ref_id", "trade_ref_group")
 
     df.show(false)
 
@@ -43,6 +52,46 @@ object CommonGroup extends SparkRunner with Logger {
 
     groupIdDF.show(false)
 
+    //updateGroupId(groupIdDF).show(false)
+
+    val counts = Seq("trade_ref_id", "trade_ref_group")
+      .foldLeft(Map[String, Long]())((counts, column) => {
+        val count = groupIdDF.select(column).distinct().count
+        counts + (column -> count)
+      })
+
+    info(s"Count : $counts")
+
+    val finalDF = checkAndUpdateGroup(groupIdDF, counts)
+
+    info("Final DF :")
+    finalDF.show(false)
+  }
+
+  @tailrec
+  def checkAndUpdateGroup(groupIdDF: DataFrame, counts: Map[String, Long]): DataFrame = {
+    if (checkCountMatch(groupIdDF, counts)) {
+      groupIdDF
+    } else {
+      info("Still to optimize grouping")
+      checkAndUpdateGroup(updateGroupId(groupIdDF), counts)
+    }
+  }
+
+  def checkCountMatch(groupIdDF: DataFrame, counts: Map[String, Long]): Boolean = {
+    info("Checking Count")
+    counts.foldLeft(true)((value, currentCheck) => {
+      if (value) {
+        val count = groupIdDF.select(col(currentCheck._1).as("id"), col("group_id")).distinct().count()
+        info(s"${currentCheck._1} Count : $count")
+        currentCheck._2 == count
+      } else {
+        value
+      }
+    })
+  }
+
+  def updateGroupId(groupIdDF: DataFrame) = {
     val groupIdDF2 = groupIdDF.groupBy("trade_ref_id")
       .agg(
         collect_set(col("trade_ref_group")).as("trade_ref_group"),
@@ -59,13 +108,12 @@ object CommonGroup extends SparkRunner with Logger {
       .withColumn("trade_ref_id", explode(col("trade_ref_id")))
       .orderBy("group_id")
 
-    groupIdDF3.show(false)
+    groupIdDF3
 
   }
-}
 
   /**
-   * 01:05:05 INFO CommonGroup$: Starting App : Sample Application
+   * Input :
    * +------------+---------------+
    * |trade_ref_id|trade_ref_group|
    * +------------+---------------+
@@ -78,75 +126,38 @@ object CommonGroup extends SparkRunner with Logger {
    * |TR6         |TG6            |
    * |TR7         |TG2            |
    * |TR8         |TG1            |
+   * |TR9         |TG9            |
+   * |TR9         |TG10           |
+   * |TR10        |TG10           |
+   * |TR10        |TG11           |
+   * |TR11        |TG11           |
+   * |TR11        |TG12           |
+   * |TR12        |TG12           |
    * +------------+---------------+
+   * Count : Map(trade_ref_id -> 12, trade_ref_group -> 7)
    *
-   * +---------------+--------------------+
-   * |trade_ref_group|trade_ref_id        |
-   * +---------------+--------------------+
-   * |TG1            |[TR4, TR1, TR3, TR8]|
-   * |TG2            |[TR7, TR1, TR5]     |
-   * |TG6            |[TR2, TR6]          |
-   * +---------------+--------------------+
-   *
-   * 01:05:21 WARN WindowExec: No Partition Defined for Window operation! Moving all data to a single partition, this can cause serious performance degradation.
+   * Output :
    * +---------------+------------+--------+
    * |trade_ref_group|trade_ref_id|group_id|
    * +---------------+------------+--------+
    * |TG1            |TR4         |1       |
    * |TG1            |TR1         |1       |
-   * |TG1            |TR3         |1       |
-   * |TG1            |TR8         |1       |
-   * |TG2            |TR7         |2       |
-   * |TG2            |TR1         |2       |
-   * |TG2            |TR5         |2       |
-   * |TG6            |TR2         |3       |
-   * |TG6            |TR6         |3       |
-   * +---------------+------------+--------+
-   *
-   * 01:05:22 WARN WindowExec: No Partition Defined for Window operation! Moving all data to a single partition, this can cause serious performance degradation.
-   * +------------+---------------+--------+
-   * |trade_ref_id|trade_ref_group|group_id|
-   * +------------+---------------+--------+
-   * |TR4         |[TG1]          |1       |
-   * |TR1         |[TG2, TG1]     |1       |
-   * |TR3         |[TG1]          |1       |
-   * |TR8         |[TG1]          |1       |
-   * |TR7         |[TG2]          |2       |
-   * |TR5         |[TG2]          |2       |
-   * |TR2         |[TG6]          |3       |
-   * |TR6         |[TG6]          |3       |
-   * +------------+---------------+--------+
-   *
-   * 01:05:23 WARN WindowExec: No Partition Defined for Window operation! Moving all data to a single partition, this can cause serious performance degradation.
-   * +------------+---------------+--------+
-   * |trade_ref_id|trade_ref_group|group_id|
-   * +------------+---------------+--------+
-   * |TR4         |TG1            |1       |
-   * |TR1         |TG2            |1       |
-   * |TR1         |TG1            |1       |
-   * |TR3         |TG1            |1       |
-   * |TR8         |TG1            |1       |
-   * |TR7         |TG2            |2       |
-   * |TR5         |TG2            |2       |
-   * |TR2         |TG6            |3       |
-   * |TR6         |TG6            |3       |
-   * +------------+---------------+--------+
-   *
-   * 01:05:24 WARN WindowExec: No Partition Defined for Window operation! Moving all data to a single partition, this can cause serious performance degradation.
-   * +---------------+------------+--------+
-   * |trade_ref_group|trade_ref_id|group_id|
-   * +---------------+------------+--------+
-   * |TG1            |TR3         |1       |
-   * |TG1            |TR4         |1       |
    * |TG2            |TR7         |1       |
    * |TG2            |TR1         |1       |
+   * |TG1            |TR3         |1       |
    * |TG2            |TR5         |1       |
    * |TG1            |TR8         |1       |
-   * |TG1            |TR1         |1       |
-   * |TG6            |TR2         |3       |
-   * |TG6            |TR6         |3       |
+   * |TG12           |TR11        |2       |
+   * |TG12           |TR12        |2       |
+   * |TG11           |TR11        |2       |
+   * |TG10           |TR9         |2       |
+   * |TG10           |TR10        |2       |
+   * |TG11           |TR10        |2       |
+   * |TG9            |TR9         |2       |
+   * |TG6            |TR2         |6       |
+   * |TG6            |TR6         |6       |
    * +---------------+------------+--------+
    *
-   * 01:05:25 INFO CommonGroup$: Application processing completed.
-   *
    */
+
+}
