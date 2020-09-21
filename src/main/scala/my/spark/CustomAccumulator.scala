e89999ad3e84c07ee6134c331d27f4a699a675ad
abhaylodha <abhaykumar.lodha@gmail.com>
2020-09-21 21:15:22 +0530

    Misc Check In

---
+++ src/main/scala/my/spark/CustomAccumulator.scala
@@ -0,0 +1,10 @@
+package my.spark
+
+import my.spark.common_utils.SparkRunner
+import org.apache.spark.sql.SparkSession
+
+object CustomAccumulator extends SparkRunner {
+  def run(spark: SparkSession, args: Array[String]): Unit = {
+    
+  }
+}
